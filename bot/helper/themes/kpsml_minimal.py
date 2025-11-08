09a504f1fce63e7c8de0cf422adde9657806155c
Ninakebots <ninakebots@gmail.com>
2025-11-08 07:23:44 +0600

    Update kpsml_minimal.py

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -2,10 +2,10 @@
 class KPSMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = '🤖 Repo'
-    ST_BN1_URL = 'https://t.me/Lord_Vasudev_Krishna'
+    ST_BN1_NAME = 'Team Wine'
+    ST_BN1_URL = 'https://t.me/Team_Wine'
     ST_BN2_NAME = 'Updates 🔥'
-    ST_BN2_URL = 'https://t.me/Lord_Vasudev_Krishna'
+    ST_BN2_URL = 'https://t.me/Soloflix_Bots'
     ST_MSG = '''<b><i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>\n
 Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
